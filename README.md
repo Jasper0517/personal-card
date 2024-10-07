6510838e92aee430f4b82bc45aba6f813eb5668a
jasper.tseng <jptseng@arnoldiii.com>
2024-10-07 14:53:22 -0700

    first time update

---
+++ README.md
@@ -0,0 +1,3 @@
+# React + Tailwind + Vite
+
+Hi there! This is a simple starter template for React + Tailwind CSS + Vite. It's a great way to get started with a new project.
---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+export default function App() {
+  return (
+    <main className="flex justify-center pt-[200px]">
+      {/* top */}
+      {/* shadow-[0px 1px 2px -1px rgb(0 0 0 / 0.10), 0px 1px 3px 0 rgb(0 0 0 / 0.10)] */}
+      <div className="flex flex-col gap-4 bg-white w-[340px] rounded-lg p-6">
+        <div className="flex">
+          <img
+            src="/assets/avatar.png"
+            alt="Jasper Photo"
+            className="w-12 h-12 rounded-full mr-5"
+          />
+          <div className="flex flex-col gap-px grow">
+            <span className="font-semibold text-lg text-neutral-900">
+              Jasper Tseng
+            </span>
+            <a
+              href="https://github.com/jasper0517"
+              target="_blank"
+              className="font-normal text-sm text-neutral-600"
+            >
+              @jasper0517
+            </a>
+          </div>
+        </div>
+        {/* bottom */}
+        <p className="font-normal text-base text-neutral-600">
+          I'm a Front-end Developer and Software Engineer currently seeking job
+          opportunities in Los Angeles. If you'd like to connect, feel free to
+          email me at{" "}
+          <a href="mailto:maple0517@gmail.com" className="text-slate-400">
+            maple0517@gmail.com
+          </a>{" "}
+          or connect with me on{" "}
+          <a
+            href="https://www.linkedin.com/in/jasper-tseng"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-slate-400"
+          >
+            LinkedIn
+          </a>
+          .
+        </p>
+      </div>
+    </main>
+  );
+}
